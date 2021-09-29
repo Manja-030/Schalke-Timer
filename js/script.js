e66084b35bd8f20645579d556fa1529e03cd96d4
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-09-29 14:39:38 +0200

    created timer function

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function startTimer() {
+  
+  let startTime = new Date().getTime();
+  let fiveMinutes = 1000 * 5;
+  let endTime = startTime + fiveMinutes;
+
+  setInterval(function() {
+    let timeLeft = endTime - new Date().getTime();
+
+    if (timeLeft > 0) {
+    
+      let minutes = timeLeft / (1000 * 60);
+      minutes = Math.floor(minutes);
+      let seconds = (timeLeft / 1000) % 60;
+      seconds = Math.round(seconds);
+    
+      let text = "0" + minutes + ":" + seconds;
+      document.getElementById("timer").innerHTML = text;
+    } else {
+      document.getElementById("timer").innerHTML = "00:00";
+    }
+
+  }, 1000);
+
+}
