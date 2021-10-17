f7a1a78bf9296379df35b803e1499dde6fd0a808
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2021-10-17 07:17:38 +0200

    updatet script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
 let alarm = new Audio("alarm.mp3");
+let timerStarted = false;
 
 function startTimer() {
-  
-  let startTime = new Date().getTime();
-  let fiveMinutes = 1000 * 5;
-  let endTime = startTime + fiveMinutes;
+  if(!timerStarted) {
 
-  setInterval(function() {
-    let timeLeft = endTime - new Date().getTime();
+    let startTime = new Date().getTime();
+    let fiveMinutes = 1000 * 60 * 5;
+    let endTime = startTime + fiveMinutes;
 
-    if (timeLeft > 0) {
-    
-      let minutes = timeLeft / (1000 * 60);
-      minutes = Math.floor(minutes);
-      let seconds = (timeLeft / 1000) % 60;
-      seconds = Math.round(seconds);
-    
-      let text = "0" + minutes + ":" + seconds;
-      document.getElementById("timer").innerHTML = text;
-    } else {
-      alarm.play();
-      document.getElementById("timer").innerHTML = "00:00";
+    setInterval(function() {
 
-    }
+      let timeLeft = endTime - new Date().getTime();
+      if (timeLeft > 0) {
+      
+        let minutes = timeLeft / (1000 * 60);
+        minutes = Math.floor(minutes);
+        let seconds = (timeLeft / 1000) % 60;
+        seconds = Math.round(seconds);
+        seconds = ("0" +  seconds).slice(-2);
+      
+        let text = "0" + minutes + ":" + seconds;
+        document.getElementById("timer").innerHTML = text;
+      
+      } else {
+        
+        alarm.play();
+        document.getElementById("timer").innerHTML = "00:00";
+        timerStarted = false;
 
-  }, 1000);
+      }
 
+    }, 1000);
+    timerStarted = true;
+  }  
 }
